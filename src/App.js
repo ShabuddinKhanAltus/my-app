7ff50f197d15e8b8a1b8e3d4b4c4fc3cb1fbe8d3
ShabuddinKhanAltus <111446712+ShabuddinKhanAltus@users.noreply.github.com>
2022-12-28 09:21:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React By Shab
+          Learn React By Shravya
         </a>
       </header>
     </div>
